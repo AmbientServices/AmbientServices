24bd04dda65d1ca69abc668e9d6490704134509b
james <james.ivie@gmail.com>
2021-01-13 20:59:41 -0700

    add DateTime extensions

---
+++ AmbientServices/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AmbientServices
+{
+    /// <summary>
+    /// A static class that extends <see cref="DateTime"/>.
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Gets the <see cref="Date"/> part of a <see cref="DateTime"/>.
+        /// </summary>
+        /// <param name="datetime">The <see cref="DateTime"/> to get the <see cref="Date"/> part for.</param>
+        /// <returns>A <see cref="Date"/> with no time portion.</returns>
+        public static Date GetDate(this DateTime datetime)
+        {
+            return Date.FromDateTime(DateTime.SpecifyKind(datetime, DateTimeKind.Unspecified));
+        }
+    }
+}
