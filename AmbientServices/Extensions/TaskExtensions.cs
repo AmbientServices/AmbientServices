b479ae7989c21586e0f96405d74a2b4a3e719a95
james <james.ivie@gmail.com>
2021-06-10 21:43:35 -0600

    add docs for TaskExtensions

--- AmbientServices/Extensions/TaskExtensions.cs
+++ AmbientServices/Extensions/TaskExtensions.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace AmbientServices
 {
+    /// <summary>
+    /// A static class that contains extensions for <see cref="Task"/>.
+    /// </summary>
     public static class TaskExtensions
     {
+        /// <summary>
+        /// Gets the specified <see cref="CancellationToken"/> as a <see cref="Task"/> that gets cancelled when the token is cancelled.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that controls the task.</param>
+        /// <returns>A <see cref="Task"/> that gets cancelled when the <see cref="CancellationToken"/> get cancelled.</returns>
         public static Task AsTask(this CancellationToken cancellationToken)
         {
             TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
