384492031e92ecdc9c9bbea52365b36a4deccf30
James <james.ivie@gmail.com>
2023-09-07 20:17:58 -0600

    add implementation of IAmbientSettingsSet that is backed by the process environment variables

---
+++ AmbientServices.Test/AlternateImplementations/TestEnvironmentSettings.cs
@@ -0,0 +1,277 @@
+﻿using AmbientServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmbientServices.Test
+{
+    /// <summary>
+    /// A class that holds tests for <see cref="IAmbientSettingsSet"/>.
+    /// </summary>
+    [TestClass]
+    public class TestEnvironmentSettings
+    {
+        private static readonly AmbientService<IAmbientSettingsSet> _SettingsSet = Ambient.GetService<IAmbientSettingsSet>();
+
+        private static readonly object _lock = new();
+
+        /// <summary>
+        /// Performs tests on <see cref="AmbientEnvironmentSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingInt()
+        {
+            // use a local override so we don't interfere with other tests
+            AmbientEnvironmentSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            using ScopedLocalServiceOverride<IAmbientSettingsSet> localOverrideTest = new(settingsSet);
+            AmbientSetting<int> setting1 = new(nameof(EnvironmentSettingInt) + "-int-setting", "", s => string.IsNullOrEmpty(s) ? 0 : Int32.Parse(s));
+            Assert.AreEqual(0, setting1.Value);
+            AmbientSetting<int> setting2 = new(nameof(EnvironmentSettingInt) + "-int-setting-2", "", s => Int32.Parse(s), "1");
+            Assert.AreEqual(1, setting2.Value);
+            AmbientSetting<int> setting3 = new(nameof(EnvironmentSettingInt) + "-int-setting-3", "", s => Int32.Parse(s), "1");
+            Assert.AreEqual(1, setting3.Value);
+
+            // test changing the setting without an event listener
+            settingsSet.ChangeSetting(setting3.Key, "5");
+            // test changing the setting to the same value without an event listener
+            settingsSet.ChangeSetting(setting3.Key, "5");
+            // test changing the setting to null so we fall through to the global settings set
+            settingsSet.ChangeSetting(setting3.Key, null);
+            int settingValue = setting3.Value;
+            Assert.AreEqual(1, setting3.Value);
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingsSetGetRawValue()
+        {
+            Environment.SetEnvironmentVariable(nameof(EnvironmentSettingsSetGetRawValue) + "_testValue", "1");
+            AmbientEnvironmentSettingsSet.Instance.Refresh();
+            AmbientEnvironmentSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            Assert.AreEqual(null, settingsSet.GetRawValue(nameof(EnvironmentSettingsSetGetRawValue) + "-notfound"));
+            Assert.AreEqual("1", settingsSet.GetRawValue(nameof(EnvironmentSettingsSetGetRawValue) + "_testValue"));
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingsRefresh()
+        {
+            AmbientEnvironmentSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            Environment.SetEnvironmentVariable(nameof(EnvironmentSettingsRefresh) + "_testValue", "1");
+            AmbientEnvironmentSettingsSet.Instance.Refresh();
+            Assert.AreEqual(null, settingsSet.GetRawValue(nameof(EnvironmentSettingsRefresh) + "-notfound"));
+            Assert.AreEqual("1", settingsSet.GetRawValue(nameof(EnvironmentSettingsRefresh) + "_testValue"));
+            Environment.SetEnvironmentVariable(nameof(EnvironmentSettingsRefresh) + "_testValue", null);
+            AmbientEnvironmentSettingsSet.Instance.Refresh();
+            Assert.AreEqual(null, settingsSet.GetRawValue(nameof(EnvironmentSettingsRefresh) + "-notfound"));
+            Assert.AreEqual(null, settingsSet.GetRawValue(nameof(EnvironmentSettingsRefresh) + "_testValue"));
+            Environment.SetEnvironmentVariable(nameof(EnvironmentSettingsRefresh) + "_testValue", "1");
+            AmbientEnvironmentSettingsSet.Instance.Refresh();
+            Assert.AreEqual(null, settingsSet.GetRawValue(nameof(EnvironmentSettingsRefresh) + "-notfound"));
+            Assert.AreEqual("1", settingsSet.GetRawValue(nameof(EnvironmentSettingsRefresh) + "_testValue"));
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingsSetNullToNull()
+        {
+            AmbientEnvironmentSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            Environment.SetEnvironmentVariable(nameof(EnvironmentSettingsSetNullToNull) + "_testValue", "1");
+            AmbientEnvironmentSettingsSet.Instance.Refresh();
+            Assert.AreEqual(null, settingsSet.GetRawValue(nameof(EnvironmentSettingsSetNullToNull) + "-notfound"));
+            Assert.AreEqual("1", settingsSet.GetRawValue(nameof(EnvironmentSettingsSetNullToNull) + "_testValue"));
+            Environment.SetEnvironmentVariable(nameof(EnvironmentSettingsSetNullToNull) + "_testValue", null);
+            AmbientEnvironmentSettingsSet.Instance.Refresh();
+            settingsSet.ChangeSetting(nameof(EnvironmentSettingsSetNullToNull) + "_testValue", null);
+            Assert.AreEqual(null, settingsSet.GetRawValue(nameof(EnvironmentSettingsSetNullToNull) + "-notfound"));
+            Assert.AreEqual(null, settingsSet.GetRawValue(nameof(EnvironmentSettingsSetNullToNull) + "_testValue"));
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingsSetSettingChangeNotification()
+        {
+            IMutableAmbientSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            string testSettingKey = nameof(EnvironmentSettingsSetSettingChangeNotification);
+            string initialValue = "initialValue";
+            string notificationNewValue = "";
+            SettingsSetSetting<string> testSetting = new(settingsSet, testSettingKey, "", s => { notificationNewValue = s; return s; }, initialValue);
+            Assert.AreEqual(initialValue, testSetting.Value);
+            string secondValue = "change1";
+            Assert.AreEqual(initialValue, notificationNewValue);
+            settingsSet.ChangeSetting(testSettingKey, secondValue);
+            Assert.AreEqual(secondValue, testSetting.Value);
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingsSetSettingChangeNoNotification()
+        {
+            IMutableAmbientSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            string testSettingKey = nameof(EnvironmentSettingsSetSettingChangeNoNotification);
+            string initialValue = "initialValue";
+            SettingsSetSetting<string> testSetting = new(settingsSet, testSettingKey, "", s => s, initialValue);
+            Assert.AreEqual(initialValue, testSetting.Value);
+            string secondValue = "change1";
+            settingsSet.ChangeSetting(testSettingKey, secondValue);
+            Assert.AreEqual(secondValue, testSetting.Value);
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingsSetSettingNullConvert()
+        {
+            IMutableAmbientSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            string testSettingKey = nameof(EnvironmentSettingsSetSettingNullConvert);
+            string initialValue = "initialValue";
+            string secondValue = "change1";
+            SettingsSetSetting<string> testSetting = new(settingsSet, testSettingKey, "", null, initialValue);
+            Assert.AreEqual(initialValue, testSetting.Value);
+            settingsSet.ChangeSetting(testSettingKey, secondValue);
+            Assert.AreEqual(secondValue, testSetting.Value);
+            settingsSet.ChangeSetting(testSettingKey, initialValue);
+            Assert.AreEqual(initialValue, testSetting.Value);
+            testSetting = new SettingsSetSetting<string>(settingsSet, testSettingKey, "", initialValue, null);
+            Assert.AreEqual(initialValue, testSetting.Value);
+            settingsSet.ChangeSetting(testSettingKey, secondValue);
+            Assert.AreEqual(secondValue, testSetting.Value);
+            settingsSet.ChangeSetting(testSettingKey, initialValue);
+            Assert.AreEqual(initialValue, testSetting.Value);
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingsSetSettingNonStringNullConvert()
+        {
+            IMutableAmbientSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            string testSettingKey = nameof(EnvironmentSettingsSetSettingNonStringNullConvert);
+            Assert.ThrowsException<ArgumentNullException>(() => new SettingsSetSetting<int>(settingsSet, testSettingKey, "", null, "1"));
+            Assert.ThrowsException<ArgumentNullException>(() => new SettingsSetSetting<int>(settingsSet, testSettingKey, "", 1, null));
+            Assert.ThrowsException<ArgumentNullException>(() => new SettingsSetSetting<int?>(settingsSet, testSettingKey, "", (int?)null, null));
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingMisc()
+        {
+            IMutableAmbientSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            Assert.IsTrue(settingsSet!.ToString()!.Contains("Environment"));
+            Assert.AreEqual("Environment", settingsSet.SetName);
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingsGarbageCollection()
+        {
+            IMutableAmbientSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            string testSettingKey = nameof(EnvironmentSettingsGarbageCollection);
+            WeakReference<SettingsSetSetting<string>> wr = FinalizableSetting(testSettingKey, settingsSet);
+            GC.Collect();   // this should collect the temporary Setting created in the function below
+            settingsSet.ChangeSetting(testSettingKey, nameof(EnvironmentSettingsGarbageCollection) + "-CauseCollect");  // this should cause the weak proxy to get removed and the instance to be destroyed
+            SettingsSetSetting<string> alive;
+            Assert.IsFalse(wr.TryGetTarget(out alive));
+        }
+        private WeakReference<SettingsSetSetting<string>> FinalizableSetting(string testSettingKey, IMutableAmbientSettingsSet settingsSet)
+        {
+            bool valueChanged = false;
+            string value = null;
+            SettingsSetSetting<string> temporarySetting = new(settingsSet, testSettingKey, "", s => { valueChanged = true; value = s; return s; }, nameof(EnvironmentSettingsGarbageCollection) + "-InitialValue");
+            WeakReference<SettingsSetSetting<string>> wr = new(temporarySetting);
+            Assert.AreEqual(nameof(EnvironmentSettingsGarbageCollection) + "-InitialValue", value);
+            // change the setting to be sure we are actually hooked into the settings set's notification event
+            settingsSet.ChangeSetting(testSettingKey, nameof(EnvironmentSettingsGarbageCollection) + "-ValueChanged");
+            Assert.AreEqual(nameof(EnvironmentSettingsGarbageCollection) + "-ValueChanged", temporarySetting.Value);
+            Assert.IsTrue(valueChanged);
+            Assert.AreEqual(nameof(EnvironmentSettingsGarbageCollection) + "-ValueChanged", value);
+            return wr;
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingLocalValueChangeNotification()
+        {
+            AmbientEnvironmentSettingsSet settingsSet = AmbientEnvironmentSettingsSet.Instance;
+            using (new ScopedLocalServiceOverride<IMutableAmbientSettingsSet>(settingsSet))
+            using (new ScopedLocalServiceOverride<IAmbientSettingsSet>(settingsSet))
+            {
+                string testSettingKey = nameof(EnvironmentSettingLocalValueChangeNotification);
+                string initialValue = "initialValue";
+                string notificationNewValue = "";
+                AmbientSetting<string> testSetting = new(testSettingKey, "", s => { notificationNewValue = s; return s; }, initialValue);
+                Assert.AreEqual(initialValue, testSetting.Value);
+                string secondValue = "change1";
+                Assert.AreEqual(initialValue, notificationNewValue);
+                settingsSet.ChangeSetting(testSettingKey, secondValue);
+                Assert.AreEqual(secondValue, testSetting.Value);
+                Assert.AreEqual(secondValue, notificationNewValue);
+            }
+        }
+        /// <summary>
+        /// Performs tests on <see cref="IAmbientSettingsSet"/>.
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentSettingsSetSettingsWithSetName()
+        {
+            string settingName = nameof(EnvironmentSettingsSetSettingsWithSetName) + "-int-setting";
+
+            IAmbientSetting<int> setting = AmbientSettings.GetSettingsSetSetting(null, settingName, "", s => Int32.Parse(s), "1");
+            Assert.AreEqual(1, setting.Value);
+            Assert.IsNotNull(setting.GetValueWithSetName().Item2);
+
+            AmbientEnvironmentSettingsSet settings = AmbientEnvironmentSettingsSet.Instance;
+            setting = AmbientSettings.GetSettingsSetSetting(settings, settingName, "", s => Int32.Parse(s), "1");
+            Assert.AreEqual(1, setting.Value);
+            Assert.IsNotNull(setting.GetValueWithSetName().Item2);
+
+            settings.ChangeSetting(settingName, "2");
+            Assert.AreEqual(2, setting.Value);
+            Assert.AreEqual("Environment", setting.GetValueWithSetName().Item2);
+        }
+        private void Registry_SettingRegistered(object sender, IAmbientSettingInfo e)
+        {
+        }
+        private void TempRegister(SettingsRegistry registry, string key = null, string description = null)
+        {
+            TestSettingsSetSetting setting2 = new(key ?? "key", "defaultValue", description);
+            registry.Register(setting2);
+        }
+
+        class TestSettingsSetSetting : IAmbientSettingInfo
+        {
+            public TestSettingsSetSetting(string key = "", string defaultValueString = null, string description = null)
+            {
+                Key = key;
+                DefaultValueString = defaultValueString;
+                Description = description;
+            }
+
+            public object DefaultValue => DefaultValueString;
+
+            public string Key { get; private set; }
+
+            public string DefaultValueString { get; private set; }
+
+            public string Description { get; private set; }
+
+            public DateTime LastUsed => DateTime.MinValue;
+
+            public object Convert(IAmbientSettingsSet settingsSet, string value)
+            {
+                return DefaultValueString;
+            }
+        }
+    }
+}
---
+++ AmbientServices/AlternateImplementations/AmbientEnvironmentSettingsSet.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace AmbientServices
+{
+    /// <summary>
+    /// A settings set that uses the process environment.
+    /// Note that since the framework does not provide an event for when environment variables change, changes after initialization will not be propagated unless those changes are made through <see cref="ChangeSetting(string, string?)"/>.
+    /// </summary>
+    public class AmbientEnvironmentSettingsSet : IMutableAmbientSettingsSet
+    {
+        private static readonly AmbientEnvironmentSettingsSet _Instance = new();
+        /// <summary>
+        /// Gets the singleton instance.
+        /// </summary>
+        public static AmbientEnvironmentSettingsSet Instance => _Instance;
+
+        private readonly LazyUnsubscribeWeakEventListenerProxy<AmbientEnvironmentSettingsSet, object?, IAmbientSettingInfo> _weakSettingRegistered;
+        private readonly ConcurrentDictionary<string, string> _rawValues;
+        private readonly ConcurrentDictionary<string, object> _typedValues;
+
+        /// <summary>
+        /// Constructs the ambient environment settings set.
+        /// </summary>
+        private AmbientEnvironmentSettingsSet()
+        {
+            _rawValues = new ConcurrentDictionary<string, string>();
+            IDictionary values = Environment.GetEnvironmentVariables();
+            _typedValues = new ConcurrentDictionary<string, object>();
+            if (values != null)
+            {
+                foreach (DictionaryEntry entry in values)
+                {
+                    _rawValues.AddOrUpdate((string)entry.Key, (string)entry.Value, (k, v) => (string)entry.Value);
+                }
+                foreach (string key in values.Keys)
+                {
+                    IAmbientSettingInfo? ps = SettingsRegistry.DefaultRegistry.TryGetSetting(key);
+                    _typedValues[key] = (ps != null) ? ps.Convert(this, (string)values[key]) : values[key];
+                }
+            }
+            _weakSettingRegistered = new LazyUnsubscribeWeakEventListenerProxy<AmbientEnvironmentSettingsSet, object?, IAmbientSettingInfo>(
+                    this, NewSettingRegistered, wvc => SettingsRegistry.DefaultRegistry.SettingRegistered -= wvc.WeakEventHandler);
+            SettingsRegistry.DefaultRegistry.SettingRegistered += _weakSettingRegistered.WeakEventHandler;
+        }
+
+        private static void NewSettingRegistered(AmbientEnvironmentSettingsSet settingsSet, object? sender, IAmbientSettingInfo setting)
+        {
+            // is there a value for this setting?
+            string? value;
+            if (settingsSet._rawValues.TryGetValue(setting.Key, out value))
+            {
+                // get the typed value
+                settingsSet._typedValues[setting.Key] = setting.Convert(settingsSet, value);
+            }
+        }
+        /// <summary>
+        /// Refreshes the settings manually by re-reading the environment variables.
+        /// </summary>
+        public void Refresh()
+        {
+            IDictionary values = Environment.GetEnvironmentVariables();
+            Dictionary<string, string> newEnvironment = new();
+            foreach (DictionaryEntry entry in values)
+            {
+                newEnvironment[(string)entry.Key] = (string)entry.Value;
+            }
+            // loop through all the old values
+            foreach (string key in _rawValues.Keys)
+            {
+                // not in the new set?
+                if (!newEnvironment.ContainsKey(key))
+                {
+                    // remove this one now because it's not longer set in the environment
+                    InternalChangeSetting(key, null);
+                }
+            }
+            // loop through all the new values
+            foreach (KeyValuePair<string, string> kvp in newEnvironment)
+            {
+                // is there *not* a value for this key or the value has changed?
+                if (!_rawValues.TryGetValue(kvp.Key, out string value) || value != kvp.Value)
+                {
+                    InternalChangeSetting(kvp.Key, kvp.Value);
+                }
+                // else ignore this environment variable, because it's the same as the value we already have
+            }
+        }
+        /// <summary>
+        /// Gets the name of the settings set so that a settings consumer can know where a changed setting value came from.
+        /// </summary>
+        public string SetName => "Environment";
+        /// <summary>
+        /// Gets the current raw (string) value for the specified key, or null if the setting is not set.
+        /// </summary>
+        /// <param name="key">A key identifying the setting whose value is to be retrieved.</param>
+        /// <returns>The setting value, or null if the setting is not set.</returns>
+
+        public string? GetRawValue(string key)
+        {
+            string? value;
+            return _rawValues.TryGetValue(key, out value) ? value : null;
+        }
+        /// <summary>
+        /// Gets the current typed value for the setting with the specified key, or null if the setting is not set.
+        /// </summary>
+        /// <param name="key">A key identifying the setting whose value is to be retrieved.</param>
+        /// <returns>The setting value, or null if the setting is not set.</returns>
+        public object? GetTypedValue(string key)
+        {
+            object? value;
+            return _typedValues.TryGetValue(key, out value) ? value : null;
+        }
+
+        /// <summary>
+        /// Changes the specified setting.
+        /// For many ambient settings services, the value will only be reflected in memory until the process shuts down, but other services may persist the change.
+        /// </summary>
+        /// <param name="key">A string that uniquely identifies the setting.</param>
+        /// <param name="value">The new string value for the setting, or null to remove the setting and revert to the .</param>
+        /// <returns>Whether or not the setting actually changed.</returns>
+        public bool ChangeSetting(string key, string? value)
+        {
+            Environment.SetEnvironmentVariable(key, value);
+            return InternalChangeSetting(key, value);
+        }
+        private bool InternalChangeSetting(string key, string? value)
+        { 
+            if (value == null)
+            {
+                string? oldValue;
+                _rawValues.TryRemove(key, out oldValue);
+                _typedValues.TryRemove(key, out _);
+                // did the value *not* change?  bail out early
+                if (oldValue == null) return false;
+            }
+            else
+            {
+                string? oldValue = null;
+                _rawValues.AddOrUpdate(key, value, (k, v) => { oldValue = v; return value; });
+                // did the value *not* change?  bail out early
+                if (string.Equals(value, oldValue, StringComparison.Ordinal)) return false;
+                IAmbientSettingInfo? ps = SettingsRegistry.DefaultRegistry.TryGetSetting(key);
+                _typedValues[key] = (ps != null) ? ps.Convert(this, value) : value;
+            }
+            return true;
+        }
+        /// <summary>
+        /// Gets a string representing the settings instance.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Settings: Environment";
+        }
+    }
+}
